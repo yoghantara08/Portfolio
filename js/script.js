fb2329a8a2cc953b3ca21900c888b31f18eec6bc
Yoghantara <gyoghantara@gmail.com>
2022-02-06 16:08:49 +0800

    menambahkan library typed js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ const nav = document.querySelector('nav ul');
 menuHamburger.addEventListener('click', function () {
   nav.classList.toggle('slide');
 });
+
+// Typed JS
+var typed = new Typed('.perkenalan p:nth-child(3)', {
+  strings: ['Front-end Developer ^1000', 'Student of ITB Stikom Bali ^1000'],
+  typeSpeed: 80,
+  backSpeed: 40,
+  loop: true,
+});
