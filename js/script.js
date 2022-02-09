a79756442a993228dee877acd49d2bd9dda57381
Yoghantara <gyoghantara@gmail.com>
2022-02-09 14:12:46 +0800

    menambahkan event klik untuk mengganti innerHTML text review my project

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ var typed = new Typed('.perkenalan p:nth-child(3)', {
 
 const container = document.querySelector('.container.project');
 const jumbo = document.querySelector('.jumbo.img-fluid');
-const thumbs = document.querySelectorAll('.thumb');
 
 container.addEventListener('click', function (e) {
   if (e.target.className == 'thumb img-fluid') {
@@ -25,4 +24,23 @@ container.addEventListener('click', function (e) {
       jumbo.classList.remove('fade');
     }, 500);
   }
+});
+
+const pStikom = document.querySelector('.pStikom');
+const pPortfolio = document.querySelector('.pPortfolio');
+const pGameSuit = document.querySelector('.pGameSuit');
+const textReviewH2 = document.querySelector('.col-lg-6.text-review h2');
+const textReviewP = document.querySelector('.col-lg-6.text-review p');
+
+pStikom.addEventListener('click', function () {
+  textReviewH2.innerHTML = 'Redesign Web Stikom Bali';
+  textReviewP.innerHTML = 'tes';
+});
+pPortfolio.addEventListener('click', function () {
+  textReviewH2.innerHTML = 'Personal Portfolio Website';
+  textReviewP.innerHTML = 'tes';
+});
+pGameSuit.addEventListener('click', function () {
+  textReviewH2.innerHTML = 'Web Game Suit';
+  textReviewP.innerHTML = 'tes';
 });
