6bb549b0a9020e85613a9d3e5f130e79c363b5b8
Yoghantara <gyoghantara@gmail.com>
2022-02-18 10:33:16 +0800

    scroll up button

--- js/script.js
+++ js/script.js
@@ -47,3 +47,17 @@ pTiga.addEventListener('click', () => {
   textReviewH2.innerHTML = 'Coming Soon...';
   textReviewP.innerHTML = '';
 });
+
+// Scroll Up
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 40) {
+      $('.go-top-btn').fadeIn();
+    } else {
+      $('.go-top-btn').fadeOut();
+    }
+  });
+  $('.go-top-btn').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 800);
+  });
+});
